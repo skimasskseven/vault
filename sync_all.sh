97ec1e83e8191b5edd25ae6d3777f450459c4bb3
mxmsolar <mxmsolar@proton.me>
2026-05-14 12:21:09 +0200

    auto-sync

--- sync_all.sh
+++ sync_all.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
-echo "Sichere Haupt-Vault..."
-cd ~/Desktop/vault && git add . && git commit -m "auto-sync" && git push
+  #!/bin/bash
+  echo "Sichere Haupt-Vault..."
+  cd ~/Desktop/vault && git add . && git commit -m "auto-sync" && git push
 
-echo "Sichere Conformis-Notizen..."
-cd ~/Desktop/vault/20_projekte/sa_conformis && git add . && git commit -m "auto-sync" && git push
+  echo "Sichere Conformis-Notizen..."
+  cd ~/Desktop/vault/20_projekte/sa_conformis && git add . && git commit -m "auto-sync" && git push
 
-echo "Sichere Running-App-Notizen..."
-cd ~/Desktop/vault/20_projekte/app_runningapp && git add . && git commit -m "auto-sync" && git push
+  echo "Sichere Running-App-Notizen..."
+  cd ~/Desktop/vault/20_projekte/app_runningapp && git add . && git commit -m "auto-sync" && git push
 
-echo "--- ALLES GESICHERT ---"
-sleep 2
+  echo "Sichere Research-Monster-Notizen..."
+  cd ~/Desktop/vault/20_projekte/research-monster && git add . && git commit -m "auto-sync" && git push
+
+  echo "--- ALLES GESICHERT ---"
+  sleep 2
+
